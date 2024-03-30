154adc5b9118f0ee7166bac43cb15f46e6cbc3db
Evgeniia <kuznetsovaevgeni@gmail.com>
2024-03-30 20:27:20 +0300

    first commit

---
+++ src/components/Special-price.js
@@ -0,0 +1,10 @@
+function SpecialPrice() {
+    return (
+        <div className="special">
+            <p className="special-price">44,50 ₽</p>
+            <p className="price-title">С картой</p>
+        </div>
+    )
+}
+
+export default SpecialPrice()
